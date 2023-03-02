57adc2615cdb0429b81679671e1f73c12399477c
Tom <tmschutter@gmail.com>
2023-03-02 15:15:23 -0700

    database init and link to server

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const postgres = require('postgres')
+
+const app = express()
+
+dotenv.config()
+
+app.use(cors('*'))
+
+const PORT = process.env.PORT
+const sql = postgres(process.env.DATABASE_URL)
+
+app.use(morgan('tiny'))
+
+app.get('/all', async (req, res) => {
+   try {
+      const data = await sql`SELECT * FROM skill`
+      res.json(data)
+   } catch (error) {
+      res.status(500).json({error})
+   }
+})
+
+app.listen(PORT, () => {
+   console.log(`listening on port: ${PORT}`);
+})
