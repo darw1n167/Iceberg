fd2cb2708cfeaadd4e3fa0e32e37b7b35610fbfb
Keyonnag <99996245+Keyonnag@users.noreply.github.com>
2023-03-07 17:04:14 -0600

    Revert "Feature   footer"

--- iceberg/src/App.jsx
+++ iceberg/src/App.jsx
@@ -1,41 +1,13 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
-import Skills from './Skills';
-import Experience from './Components/Experience';
-import Education from './Components/Education';
-import ActivityFeed from './Components/Acitivity';
-import About from './components/About';
-import Footer from './Components/footer';
+import './App.css'
 
-export const ExperienceContext = React.createContext();
-
-const API_URL = 'http://localhost:8000';
 
 function App() {
-	const [experience, setExperience] = useState([]);
 
-	useEffect(() => {
-		async function fetchExperience() {
-			const response = await fetch(`${API_URL}/profile`);
-			const data = await response.json();
-			setExperience(data);
-			console.log(data);
-		}
-		fetchExperience();
-	}, []);
-	return (
-		<>
-			<ExperienceContext.Provider value={{ experience, setExperience }}>
-				<Experience />
-				<ActivityFeed />
-				<Education />
-				<About />
-				<Skills />
-				<Footer />
-			</ExperienceContext.Provider>
-		</>
-	);
+  return (
+    <>
+      <div>hi</div>
+    </>
+  )
 }
 
-export default App;
+export default App
