15ad887c9d1b5791c81a6caddd96152714be7ef3
RobinsonNguyen <robinson_nguyen@hotmail.com>
2023-03-09 05:30:51 +0900

    added tooltips on notify badge

--- iceberg/src/Components/Profile.jsx
+++ iceberg/src/Components/Profile.jsx
@@ -42,7 +42,10 @@ function ProfilePhoto() {
         </div>
         <div className="flex-1 flex-column">
           <div className="badges">
-            <span className="individual-badge premium-color">
+            <span
+              className="individual-badge premium-color"
+              title="Jacob is a Bad Ass"
+            >
               <li-icon>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -63,7 +66,7 @@ function ProfilePhoto() {
                 </svg>
               </li-icon>
             </span>
-            <span className="individual-badge">
+            <span className="individual-badge relative">
               <button className="notify-button">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -78,6 +81,16 @@ function ProfilePhoto() {
                   <path d="M22 19h-8.28a2 2 0 11-3.44 0H2v-1a4.52 4.52 0 011.17-2.83l1-1.17h15.7l1 1.17A4.42 4.42 0 0122 18zM18.21 7.44A6.27 6.27 0 0012 2a6.27 6.27 0 00-6.21 5.44L5 13h14z"></path>
                 </svg>
               </button>
+              <div
+                tabIndex="-1"
+                role="tooltip"
+                placement="bottom"
+                className="hoverable-notify absolute"
+              >
+                <div className="padding-8-12">
+                  <div>Only get notified about Jacob's top posts.</div>
+                </div>
+              </div>
             </span>
           </div>
         </div>
