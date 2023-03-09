3637811d5b798ec6d88770ac4bc40cb33c697214
bhernandezortiz <neybrian11@gmail.com>
2023-03-09 12:51:11 -0800

    needs a pop-up on the me nav-item

--- iceberg/src/Components/NavBar.jsx
+++ iceberg/src/Components/NavBar.jsx
@@ -11,14 +11,14 @@ const NavBar = () => {
             </svg>
       </div>
       <div className="search-box">
-            {/* <div className="insideSearch">
-                <div className="insideinsideSearch"> */}
+                <div>
+                    <input className="search-input" type="text" placeholder="Search" />
+                </div>
+                <div className="search-icon">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" data-supported-dps="16x16" fill="currentColor" className="mercado-match" width="16" height="16" focusable="false">
                     <path d="M14.56 12.44L11.3 9.18a5.51 5.51 0 10-2.12 2.12l3.26 3.26a1.5 1.5 0 102.12-2.12zM3 6.5A3.5 3.5 0 116.5 10 3.5 3.5 0 013 6.5z"></path>
                     </svg>
-                    <input className="search-input" type="text" placeholder="Search" />
-                {/* </div>
-            </div> */}
+                </div>
         </div>
         <div className="nav-links">
             <div className="home">
